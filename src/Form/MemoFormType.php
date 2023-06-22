22e957b0acf92962a475cc93f8a7ccc539805147
Makoto Ikami <makoto.ikami@gmail.com>
2023-06-22 17:52:40 +0900

    Memo Formを作成

---
+++ src/Form/MemoFormType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Memo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MemoFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('content', TextareaType::class, [
+                'label' => 'Content',
+                'attr' => ['rows' => 5],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Memo::class,
+        ]);
+    }
+}
